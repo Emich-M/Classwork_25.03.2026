d46cc067dedf92d22dd00ca7eb5ca111260c9f3a
Emich-M <fagetolrer@gmail.com>
2026-03-25 14:52:47 +0300

    construction/destruction tests

--- pe-vector.hpp
+++ pe-vector.hpp
@@ -1,12 +1,16 @@
 #ifndef PE_VECTOR_HPP
 #define PE_VECTOR_HPP
-#include <csttdef>
-namespace knk
-{
+#include <cstddef>
+namespace knk{
 	template< class T >
 	struct Vector {
+	public:
+		~Vector();
+		Vector();
+
+	private:
 		T* data;
-		size_t size, capacity;
+		size_t size_, capacity_;
 	};
 }
 #endif
